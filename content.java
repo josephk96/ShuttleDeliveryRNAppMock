113a7906f1f5b25db83b96a72a6c1b3b33190003
Soo Hwan Kim <kimsoohwan10@gmail.com>
2021-10-08 01:27:01 +0000

    add code in files types: .gradle,.java,.lock,.pbxproj,.json,.tsx,.ts,.tsx} remove code in files types: .gradle,.lock,.pbxproj,.json,.tsx,.ts,.tsx}

--- content.java
+++ content.java
@@ -5,5 +5,6 @@ public class Ciqqny {
         System.out.println("pwihw");
         System.out.println("absgh");
         System.out.println("apvym");
+        System.out.println("cjfnc");
     }
 }
